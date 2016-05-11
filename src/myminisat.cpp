233b1252a875c5c01039646cbc4eeb0a8ad3e319
Miklos Maroti <mmaroti@gmail.com>
2016-05-11 16:49:38 +0200

    first commit

---
+++ src/myminisat.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : myminisat.cpp
+// Author      : Miklos Maroti
+// Version     :
+// Copyright   : MIT
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!");
+	return EXIT_SUCCESS;
+}
